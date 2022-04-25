57e984daa1adc3f8bf198094496915cba8a95728
AdeyEudora <pedronardes2004@gmail.com>
2022-04-25 17:21:32 -0300

    Tema Wordpress APW

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>EU NÃO SEI</title>
+</head>
+<body>
+	<h1>Olá mundo</h1>
+	<h2>Meu primeiro tema</h2>
+	<h3>Feito por: Pedro Henrique dos Santos Nardes</h3>
+</body>
+</html>
