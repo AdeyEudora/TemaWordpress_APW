d5b3e536b1de7f3db6ec2b5b2c4b45678605bced
AdeyEudora <51864886+AdeyEudora@users.noreply.github.com>
2022-05-09 22:44:48 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie-edge">
-	<title><?php wp_tittle();?></title>
+	<title>WP TITLE TÁ DANDO ERRO</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri()?>">
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url');?>">
 </head>
